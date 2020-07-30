481165914259d68ae73f77556be6b887def0285c
xsh <xsh2668028614@qq.com>
2020-07-31 00:11:44 +0800

    LogAspect日志切面类修改，记录每次访问的ip,接口地址，耗时，入参，返回结果，异常

--- src/main/java/com/xsh/aspect/LogAspect.java
+++ src/main/java/com/xsh/aspect/LogAspect.java
@@ -1,84 +1,238 @@
 package com.xsh.aspect;
 
-import com.xsh.util.GetAddressByIpUtils;
+import com.alibaba.fastjson.JSON;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author : xsh
- * @create : 2020-02-06 - 23:32
- * @describe: 日志切面类
+ * @create : 2020-07-30 - 21:37
+ * @describe: 日志切面类改进，记录每一次访问的接口和参数 ip地址 异常 访问耗时
+ * 接口参数与返回结果不记录，如需记录则打开49，50行的注释的就行
  */
-@Aspect
 @Component
+@Aspect
 public class LogAspect {
+    private final static Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
 
-    private final Logger logger= LoggerFactory.getLogger(this.getClass());
-    @Pointcut("execution(* com.xsh.web.*.*(..))")
-    public void log(){
-
+    @Pointcut("execution(* com.xsh.web..*(..))")
+    public void requestServer() {
     }
 
-    @Before("log()")
-    public void doBefore(JoinPoint joinPoint){
-        logger.info("------doBefore------");
-        //获取访问者ip地址,访问的url,访问的方法和参数
+    @Around("requestServer()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Object result = proceedingJoinPoint.proceed();
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setIp(request.getRemoteAddr());
+        requestInfo.setUrl(request.getRequestURL().toString());
+        requestInfo.setHttpMethod(request.getMethod());
+        requestInfo.setClassMethod(String.format("%s.%s", proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName()));
+        //requestInfo.setRequestParams(getRequestParamsByProceedingJoinPoint(proceedingJoinPoint));
+        //requestInfo.setResult(result);
+        requestInfo.setResult(null);//retult,RequestParams内容太多，不记录日志
+        requestInfo.setRequestParams(null);
+        requestInfo.setTimeCost(System.currentTimeMillis() - start);
+        LOGGER.info("Request Info : {}", JSON.toJSONString(requestInfo));
 
-        //todo
-/*        ServletRequestAttributes attributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request=attributes.getRequest();
-        String url=request.getRequestURL().toString();
-        String ip=request.getRemoteAddr();
-        String classMethod=joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName();
-        String address = GetAddressByIpUtils.ipToAddress(ip);
-        Object[] args =joinPoint.getArgs();
-        RequestLog requestLog=new RequestLog(url,ip,classMethod,args);
-        logger.info("访问者地址：{}",address);
-        logger.info("Request : {}",requestLog);*/
+        return result;
     }
 
-    @After("log()")
-    public void doAfter(){
-        logger.info("------doAfter------");
+
+    @AfterThrowing(pointcut = "requestServer()", throwing = "e")
+    public void doAfterThrow(JoinPoint joinPoint, RuntimeException e) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        RequestErrorInfo requestErrorInfo = new RequestErrorInfo();
+        requestErrorInfo.setIp(request.getRemoteAddr());
+        requestErrorInfo.setUrl(request.getRequestURL().toString());
+        requestErrorInfo.setHttpMethod(request.getMethod());
+        requestErrorInfo.setClassMethod(String.format("%s.%s", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName()));
+        requestErrorInfo.setRequestParams(getRequestParamsByJoinPoint(joinPoint));
+        requestErrorInfo.setException(e);
+        LOGGER.info("Error Request Info : {}", JSON.toJSONString(requestErrorInfo));
     }
 
-    @AfterReturning(pointcut = "log()",returning = "result")
-    public void doAfterReturn(Object result){
-        logger.info("Result : {}",result);
+    /**
+     * 获取入参
+     *
+     * @param proceedingJoinPoint
+     * @return
+     */
+    private Map<String, Object> getRequestParamsByProceedingJoinPoint(ProceedingJoinPoint proceedingJoinPoint) {
+        //参数名
+        String[] paramNames = ((MethodSignature) proceedingJoinPoint.getSignature()).getParameterNames();
+        //参数值
+        Object[] paramValues = proceedingJoinPoint.getArgs();
+
+        return buildRequestParam(paramNames, paramValues);
     }
 
-    public class RequestLog{
-        private String url;
-        private String ip;
-        private String ClassMethod;
-        private  Object[] args;
+    private Map<String, Object> getRequestParamsByJoinPoint(JoinPoint joinPoint) {
+        //参数名
+        String[] paramNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+        //参数值
+        Object[] paramValues = joinPoint.getArgs();
 
-        public RequestLog(String url, String ip, String classMethod, Object[] args) {
-            this.url = url;
-            this.ip = ip;
-            ClassMethod = classMethod;
-            this.args = args;
+        return buildRequestParam(paramNames, paramValues);
+    }
+
+    private Map<String, Object> buildRequestParam(String[] paramNames, Object[] paramValues) {
+        Map<String, Object> requestParams = new HashMap<>();
+        for (int i = 0; i < paramNames.length; i++) {
+            Object value = paramValues[i];
+
+            //如果是文件对象
+            if (value instanceof MultipartFile) {
+                MultipartFile file = (MultipartFile) value;
+                value = file.getOriginalFilename(); //获取文件名
+            }
+
+            requestParams.put(paramNames[i], value);
         }
 
-        @Override
-        public String toString() {
-            return "RequestLog{" +
-                    "url='" + url + '\'' +
-                    ", ip='" + ip + '\'' +
-                    ", ClassMethod='" + ClassMethod + '\'' +
-                    ", args=" + Arrays.toString(args) +
-                    '}';
+        return requestParams;
+    }
+
+    public class RequestInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private Object result;
+        private Long timeCost;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getHttpMethod() {
+            return httpMethod;
+        }
+
+        public void setHttpMethod(String httpMethod) {
+            this.httpMethod = httpMethod;
+        }
+
+        public String getClassMethod() {
+            return classMethod;
+        }
+
+        public void setClassMethod(String classMethod) {
+            this.classMethod = classMethod;
+        }
+
+        public Object getRequestParams() {
+            return requestParams;
+        }
+
+        public void setRequestParams(Object requestParams) {
+            this.requestParams = requestParams;
+        }
+
+        public Object getResult() {
+            return result;
+        }
+
+        public void setResult(Object result) {
+            this.result = result;
+        }
+
+        public Long getTimeCost() {
+            return timeCost;
+        }
+
+        public void setTimeCost(Long timeCost) {
+            this.timeCost = timeCost;
+        }
+    }
+
+    public class RequestErrorInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private RuntimeException exception;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getHttpMethod() {
+            return httpMethod;
+        }
+
+        public void setHttpMethod(String httpMethod) {
+            this.httpMethod = httpMethod;
+        }
+
+        public String getClassMethod() {
+            return classMethod;
+        }
+
+        public void setClassMethod(String classMethod) {
+            this.classMethod = classMethod;
+        }
+
+        public Object getRequestParams() {
+            return requestParams;
+        }
+
+        public void setRequestParams(Object requestParams) {
+            this.requestParams = requestParams;
+        }
+
+        public RuntimeException getException() {
+            return exception;
+        }
+
+        public void setException(RuntimeException exception) {
+            this.exception = exception;
         }
     }
 }
